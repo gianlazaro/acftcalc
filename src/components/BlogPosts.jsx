61e611def210f70a7c202da23cbac82ac4e39beb
Gian Lazaro <hello@gianlazaro.com>
2022-03-31 11:08:45 -0700

    Optimize youtube thumbnails

--- src/components/BlogPosts.jsx
+++ src/components/BlogPosts.jsx
@@ -16,6 +16,8 @@ export const BlogPosts = [
         mute="false"
         lazyImage="true"
         iframeTitle="Key Changes to the ACFT Announced"
+        mobileResolution="sddefault"
+        desktopResolution="sddefault"
       />,
       "The strength deadlift event will require soldiers to lift a weight from the ground to waist height in one smooth motion. The standing power throw event will test the soldier's ability to throw a weighted ball as far as possible. The hand-release pushups event will measure upper body strength and endurance. For the sprint/drag/carry event, soldiers will have to sprint 100 yards, drag a weight sled for 50 yards, and carry weight for 100 yards. The planks event will test the soldier's abdominal strength and endurance. And finally, the two-mile run will measure the soldier's aerobic capacity.",
 
@@ -24,6 +26,8 @@ export const BlogPosts = [
         mute="false"
         lazyImage="true"
         iframeTitle="ACFT Event 5- Plank (PLK)"
+        mobileResolution="sddefault"
+        desktopResolution="sddefault"
       />,
       "The ACFT is an important step in improving the physical fitness of soldiers. The old APFT was not very effective in assessing all aspects of physical fitness, whereas the new ACFT will do just that. The ACFT is also more challenging than the APFT, so it will better prepare soldiers for combat situations. Overall, the Army ACFT is a much-needed update to the current fitness assessment process!",
       "Photo credits: U.S. Army photo by Sgt. Inki Chung, Defense Visual Information Distribution Service",
@@ -44,6 +48,8 @@ export const BlogPosts = [
         mute="false"
         lazyImage="true"
         iframeTitle="ACFT Event 5- Plank (PLK)"
+        mobileResolution="sddefault"
+        desktopResolution="sddefault"
       />,
       "Static exercises such as planks can cause muscles to tighten up, which can lead to injuries like shin splints or even stress fractures. In addition, if you do planks before running, you’re more likely to experience discomfort in your lower back and hips.",
       "Running is a high-impact activity, and when you combine it with a static exercise like planks, you’re asking for trouble. Instead of doing planks in the ACFT, focus on dynamic exercises that will prepare your body for the run.",
@@ -67,6 +73,8 @@ export const BlogPosts = [
         mute="false"
         lazyImage="true"
         iframeTitle="SMA Talks New ACFT Updates"
+        mobileResolution="sddefault"
+        desktopResolution="sddefault"
       />,
       "There are a few reasons for this change. First, the Army has realized that returning to these standards will help to ensure that all soldiers are physically capable of meeting the demands of their job. Second, the Army believes that returning to these standards will help to reduce injuries among soldiers. Third, the Army feels that returning to these standards is important for morale and cohesion within the force.",
       "While some people may be unhappy with this decision, I believe that it is the right one. The Army has always been a physically demanding organization, and it is important that our soldiers are able to meet those demands. Returning to gender and age standards will help ensure that all soldiers are physically capable of meeting those demands, which in turn will make us a stronger and more effective force. Thanks for reading!",
