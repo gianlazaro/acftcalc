db36d32b24f065998f407928c143073322ca091d
Gian Lazaro <hello@gianlazaro.com>
2022-03-30 20:14:08 -0700

    Remove Google Analytics and Google Adsense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,7 +32,7 @@ function App() {
         <div></div>
         <BlogWidget>
           <h2 style={{ borderBottom: "1px solid #333", paddingBottom: "1rem" }}>
-            Blog Posts 📝
+            Blog Posts
           </h2>
           <ul>{blogTitles}</ul>
         </BlogWidget>
